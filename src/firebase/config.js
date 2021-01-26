bcddca30e53307bfe34bac6ab090f9974e5b7127
Nori <loveandfight2186@yahoo.co.jp>
2021-01-26 16:27:39 +0900

    create config files for firebase auth

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyAETHSNku0U9r-V4pEJg5RdD9Eu0qRReto",
+  authDomain: "cheers-c2f76.firebaseapp.com",
+  projectId: "cheers-c2f76",
+  storageBucket: "cheers-c2f76.appspot.com",
+  messagingSenderId: "872438784518",
+  appId: "1:872438784518:web:ad6b841b6b3d182ca49987",
+  measurementId: "G-65HNREG826"
+};
