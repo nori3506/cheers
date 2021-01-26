476f973367b274cd71b43ca56bac749cce39218a
Nori <loveandfight2186@yahoo.co.jp>
2021-01-27 01:29:43 +0900

    implement login logout signup

---
+++ src/components/Home.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { Card, Button, Alert } from 'react-bootstrap'
+import { useAuth } from '../contexts/AuthContext'
+import { Link, useHistory } from 'react-router-dom'
+
+export default function Home() {
+  const [error, setError] = useState("")
+  const { currentUser, logout } = useAuth()
+  const history = useHistory()
+  
+  async function handleLogout() {
+    setError('')
+    try {
+      await logout()
+      history.pushState('/login')
+    } catch {
+      setError('Failed to Log out')
+    }
+  }
+
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          {currentUser.email}
+          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">Profile Update</Link>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Button variant="link" onClick={handleLogout}>Log Out</Button>
+      </div>
+    </>
+  )
+}
