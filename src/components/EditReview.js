f7491fe67d364b53e54dd9a9c0f3b2027f244f7f
Nori <loveandfight2186@yahoo.co.jp>
2021-02-23 02:24:11 +0900

    implement base EditReview

--- src/components/EditReview.js
+++ src/components/EditReview.js
@@ -2,14 +2,45 @@ import React, { useState, useEffect } from 'react'
 import { db, strage } from '../firebase/index'
 import { useAuth } from '../contexts/AuthContext'
 import { Form, Button, Alert, Tabs, Tab, } from 'react-bootstrap'
-
-
+import { SelectInput, TextInput } from './UIkit';
 
 export default function EditReview() {
+  const [review, setReview] = useState("")
+  const [drinkName, setdrinkName] = useState("")
+  let review_id = window.location.pathname.split('/review/edit/', 2)[1]
+
+  useEffect(() => {
+    const reviewRef = db.collection('reviews').doc(review_id)
+    reviewRef.get().then(doc => {
+      setReview(doc.data())
+      setdrinkName(doc.data().drink_name)
+    })
+  }, [])
+
+  const updateReview = () => {
+    console.log("hogehoge")
+  }
+
+  const aaa = () => {
+    console.log("hoge")
+  }
 
   return (
-    <>
-      aaaaa
+    <> 
+      <Form onSubmit={updateReview}>
+        <TextInput
+          fullWidth={true} label={"Drink name"} multiline={false} required={true}
+          rows={1} value={drinkName} type={"text"} onChange={aaa} 
+        />
+        
+        <br />
+        <Button
+          className="w-100"
+          type="submit"
+          variant="primary"
+        >Update
+        </Button>
+      </Form>
     </>
   )
 }
