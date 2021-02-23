c6e98e7d4c01efe70975cc08e97fc1e64b3e5cc4
Nori <loveandfight2186@yahoo.co.jp>
2021-02-24 00:28:40 +0900

    modify indent

--- src/components/DisplayReview.js
+++ src/components/DisplayReview.js
@@ -3,40 +3,40 @@ import { db } from '../firebase/index';
 import React, {useState, useEffect} from 'react';
 
 const DisplayReview = () => {
-	const [reviews, setReviews] = useState([]);
-	let pathName = window.location.pathname;
-	let fbPathName = pathName.replace("/shop", "shops")
-	let shopRef = db.doc(fbPathName);
-	let userRef = db.collection('users')
+  const [reviews, setReviews] = useState([]);
+  let pathName = window.location.pathname;
+  let fbPathName = pathName.replace("/shop", "shops")
+  let shopRef = db.doc(fbPathName);
+  let userRef = db.collection('users')
 
-	useEffect(() => {
-		shopRef.get().then(function(shop) {
-			if (shop.exists) {
-			db.collection("reviews").where("shop", "==", shopRef)
-			.get()
-			.then(function(querySnapshot) {
-				querySnapshot.forEach(function(review) {
-					userRef.doc(review.data().user.id).get().then(snapshot => {
+  useEffect(() => {
+    shopRef.get().then(function(shop) {
+      if (shop.exists) {
+      db.collection("reviews").where("shop", "==", shopRef)
+      .get()
+      .then(function(querySnapshot) {
+        querySnapshot.forEach(function(review) {
+          userRef.doc(review.data().user.id).get().then(snapshot => {
             setReviews((reviews) => [...reviews, { ...review.data(), shop: shop.data(), user: snapshot.data() }])
-					}).catch(function(error) {
-					 	console.log("Error getting document:", error);
-					});										 
-				});
-			})
-		} else {
-			console.log("No such document!");
-		}
-	}).catch(function(error) {
-		console.log("Error getting document:", error);
-	});
-	
-	}, [])
+          }).catch(function(error) {
+             console.log("Error getting document:", error);
+          });										 
+        });
+      })
+    } else {
+      console.log("No such document!");
+    }
+  }).catch(function(error) {
+    console.log("Error getting document:", error);
+  });
+  
+  }, [])
 
-	const reviewItems = reviews.map((review, i) => {
+  const reviewItems = reviews.map((review, i) => {
     return (
       <div className="reviews-background reviews-area">
-				<h2 className="u-text-small">{review.drink_name}</h2>
-				<li>{review.user.name}</li>
+        <h2 className="u-text-small">{review.drink_name}</h2>
+        <li>{review.user.name}</li>
         <li>{review.price} CAD</li>
         <li>{review.rating}</li>
         <li>{review.drink_category}</li>
