4ab8a4890e6d468ce0f4396a5055e6bbcc6029cf
Shinya Sato <0113shin.s@gmail.com>
2021-03-09 17:58:43 -0800

    ass style

--- src/components/DisplayReview.js
+++ src/components/DisplayReview.js
@@ -1,72 +1,84 @@
 //import React from "react";
-import { db } from '../firebase/index';
-import React, {useState, useEffect} from 'react';
+import { db } from '../firebase/index'
+import React, { useState, useEffect } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import { Link } from 'react-router-dom'
 import { ButtonInput } from './UIkit'
 
 const DisplayReview = () => {
-  const [reviews, setReviews] = useState([]);
-  const [orderedReviews, setOrderedReviews] = useState([]);
-  const pathName = window.location.pathname;
-  const fbPathName = pathName.replace("/shop", "shops")
+  const [reviews, setReviews] = useState([])
+  const [orderedReviews, setOrderedReviews] = useState([])
+  const pathName = window.location.pathname
+  const fbPathName = pathName.replace('/shop', 'shops')
   const { currentUser } = useAuth()
-  const shopRef = db.doc(fbPathName);
+  const shopRef = db.doc(fbPathName)
   const userRef = db.collection('users')
 
   useEffect(() => {
-        shopRef.get().then(function(shop) {
-          if (shop.exists) {
-          db.collection("reviews").where("shop", "==", shopRef)
-          .get()
-          .then(function(querySnapshot) {
-            querySnapshot.forEach(function(review) {
-              userRef.doc(review.data().user.id)
-              .get()
-              .then(snapshot => {           
-                setReviews((reviews) => [...reviews, { ...review.data(), shop: shop.data(), user: snapshot, ref: review.ref }])
-              }).catch(function(error) {
-                 console.log("Error getting document:", error);
-              });										 
-            });
-          })
+    shopRef
+      .get()
+      .then(function (shop) {
+        if (shop.exists) {
+          db.collection('reviews')
+            .where('shop', '==', shopRef)
+            .get()
+            .then(function (querySnapshot) {
+              querySnapshot.forEach(function (review) {
+                userRef
+                  .doc(review.data().user.id)
+                  .get()
+                  .then(snapshot => {
+                    setReviews(reviews => [
+                      ...reviews,
+                      { ...review.data(), shop: shop.data(), user: snapshot, ref: review.ref },
+                    ])
+                  })
+                  .catch(function (error) {
+                    console.log('Error getting document:', error)
+                  })
+              })
+            })
         } else {
-          console.log("No such document!");
+          console.log('No such document!')
         }
-      }).catch(function(error) {
-        console.log("Error getting document:", error);
-      });
+      })
+      .catch(function (error) {
+        console.log('Error getting document:', error)
+      })
   }, [])
 
   function handleDelete(review) {
     const promises = []
     if (window.confirm('Are you Sure to Delete This Review?')) {
-      db.collection('reviews').get().then((snapshot) => {
-         snapshot.forEach(doc => {
-           if (doc.id === review.ref.id) {
-             promises.push(db.collection('reviews').doc(doc.id).delete());
-             Promise.all(promises).then(() => {
-              const newReviews = reviews.filter(review => review.ref.id !== doc.id)
-              setReviews(newReviews)
-              const shopDocRef = db.collection('shops').doc(doc.data().shop.id)
-              let query = db.collection('reviews').where("shop", "==", shopDocRef)
-              query.get().then(querySnapshot => {
-                if (querySnapshot.empty) {
-                  shopDocRef.delete()
-                }
+      db.collection('reviews')
+        .get()
+        .then(snapshot => {
+          snapshot.forEach(doc => {
+            if (doc.id === review.ref.id) {
+              promises.push(db.collection('reviews').doc(doc.id).delete())
+              Promise.all(promises).then(() => {
+                const newReviews = reviews.filter(review => review.ref.id !== doc.id)
+                setReviews(newReviews)
+                const shopDocRef = db.collection('shops').doc(doc.data().shop.id)
+                let query = db.collection('reviews').where('shop', '==', shopDocRef)
+                query
+                  .get()
+                  .then(querySnapshot => {
+                    if (querySnapshot.empty) {
+                      shopDocRef.delete()
+                    }
+                  })
+                  .catch(function (error) {
+                    console.log('Error getting documents: ', error)
+                  })
               })
-              .catch(function (error) {
-                console.log("Error getting documents: ", error);
-              })
-            })
-          }
+            }
+          })
         })
-      })
     }
   }
 
-
-  const reviewItems = reviews.map((review) => {
+  const reviewItems = reviews.map(review => {
     return (
       <div className="reviews-background reviews-area">
         <h2 className="u-text-small">{review.drink_name}</h2>
@@ -79,8 +91,10 @@ const DisplayReview = () => {
           if (currentUser.uid == review.user.id) {
             return (
               <>
-                <Link to={'/review/edit/' + review.ref.id} className="blue-color">Edit</Link>
-                <ButtonInput label={"Delete"} onClick={() => handleDelete(review)} />
+                <Link to={'/review/edit/' + review.ref.id} className="blue-color">
+                  Edit
+                </Link>
+                <ButtonInput label={'Delete'} onClick={() => handleDelete(review)} />
               </>
             )
           }
@@ -89,18 +103,10 @@ const DisplayReview = () => {
     )
   })
   if (reviewItems.length) {
-    return (
-      <ul>
-        {reviewItems}
-      </ul>
-    )
+    return <ul className="review-list">{reviewItems}</ul>
   } else {
-    return(
-      <div className="reviews-background u-text-center">
-      No reviews are found
-      </div>
-    )
+    return <div className="reviews-background u-text-center">No reviews are found</div>
   }
 }
 
-export default DisplayReview;
+export default DisplayReview
--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import { Link } from 'react-router-dom'
 import { ButtonInput } from './UIkit'
@@ -10,15 +10,16 @@ const Reviews = () => {
   const { currentUser } = useAuth()
 
   useEffect(() => {
-    reviewsRef.get().then((snapshot) => {
-      snapshot.forEach((review) => {
+    reviewsRef.get().then(snapshot => {
+      snapshot.forEach(review => {
         if (review?.data()?.user?.id == currentUser.uid) {
-          db.collection('shops').doc(review.data().shop.id).get().then(
-            snapshot => {
+          db.collection('shops')
+            .doc(review.data().shop.id)
+            .get()
+            .then(snapshot => {
               const shop = snapshot.data()
-              setReviews((reviews) => [...reviews, { ref: review.ref, ...review.data(), shop: shop }])
-            }
-          )
+              setReviews(reviews => [...reviews, { ref: review.ref, ...review.data(), shop: shop }])
+            })
         }
       })
     })
@@ -27,56 +28,56 @@ const Reviews = () => {
   function handleDelete(review) {
     const promises = []
     if (window.confirm('Are you Sure to Delete This Review?')) {
-      db.collection('reviews').get().then((snapshot) => {
-         snapshot.forEach(doc => {
-           if (doc.id === review.ref.id) {
-             promises.push(db.collection('reviews').doc(doc.id).delete());
-             Promise.all(promises).then(() => {
-              const newReviews = reviews.filter(review => review.ref.id !== doc.id)
-              setReviews(newReviews)
-              const shopDocRef = db.collection('shops').doc(doc.data().shop.id)
-              let query = db.collection('reviews').where("shop", "==", shopDocRef)
-              query.get().then(querySnapshot => {
-                if (querySnapshot.empty) {
-                  shopDocRef.delete()
-                }
+      db.collection('reviews')
+        .get()
+        .then(snapshot => {
+          snapshot.forEach(doc => {
+            if (doc.id === review.ref.id) {
+              promises.push(db.collection('reviews').doc(doc.id).delete())
+              Promise.all(promises).then(() => {
+                const newReviews = reviews.filter(review => review.ref.id !== doc.id)
+                setReviews(newReviews)
+                const shopDocRef = db.collection('shops').doc(doc.data().shop.id)
+                let query = db.collection('reviews').where('shop', '==', shopDocRef)
+                query
+                  .get()
+                  .then(querySnapshot => {
+                    if (querySnapshot.empty) {
+                      shopDocRef.delete()
+                    }
+                  })
+                  .catch(function (error) {
+                    console.log('Error getting documents: ', error)
+                  })
               })
-              .catch(function (error) {
-                console.log("Error getting documents: ", error);
-              })
-            })
-          }
+            }
+          })
         })
-      })
     }
   }
-  
+
   const reviewItems = reviews.map((review, i) => {
     return (
       <div className="reviews-background reviews-area">
-        <h2 className="u-text-small">{review.drink_name} <span className="normal-font-weight">at</span> {review.shop.name}</h2>
+        <h2 className="u-text-small">
+          {review.drink_name} <span className="normal-font-weight">at</span> {review.shop.name}
+        </h2>
         <li>{review.price} CAD</li>
         <li>{review.rating}</li>
         <li>{review.drink_category}</li>
         <p>"{review.comment}"</p>
-        <Link to={'/review/edit/' + review.ref.id} className="blue-color">Edit</Link>
-        <ButtonInput label={"Delete"} onClick={() =>handleDelete(review)}/>
+        <Link to={'/review/edit/' + review.ref.id} className="blue-color">
+          Edit
+        </Link>
+        <ButtonInput label={'Delete'} onClick={() => handleDelete(review)} />
       </div>
     )
   })
   if (reviewItems.length) {
-    return (
-      <ul>
-        {reviewItems}
-      </ul>
-    )
+    return <ul className="review-list">{reviewItems}</ul>
   } else {
-    return(
-      <div className="reviews-background u-text-center">
-        Your Reviews were not found
-      </div>
-    )
+    return <div className="reviews-background u-text-center">Your Reviews were not found</div>
   }
 }
 
-export default Reviews;
+export default Reviews
