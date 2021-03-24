adeb42024095aa46b3699b1aa4e462233930e087
azusanakahashi <azupin86@gmail.com>
2021-03-23 22:15:15 -0700

    css mss modification

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react'
 import { Form, Card, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
 import { Link, useHistory } from 'react-router-dom'
+import logo from '../assets/icons/logo.svg'
 
 export default function Login() {
   const nameRef = useRef()
@@ -28,28 +29,33 @@ export default function Login() {
 
   return (
     <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Log In</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={hanleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required className="input--lg" />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required className="input--lg" />
-            </Form.Group>
-            <button type="submit" disabled={loading} className="btn--primary">
-              Login
-            </button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        New to use? <Link to="/signup">Sign Up</Link>
+      <div class="login-wrapper">
+        <Card>
+          <Card.Body id="login">
+            <h2 className="text-center mb-4 visually-hidden">Log In</h2>
+            <img src={logo}></img>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={hanleSubmit}>
+              <Form.Group id="email">
+              <Form.Label className="visually-hidden">Email</Form.Label>
+                <Form.Control placeholder="Email"  type="email" ref={emailRef} required className="input--lg" />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label className="visually-hidden">Password</Form.Label>
+                <Form.Control placeholder="Password" type="password" ref={passwordRef} required className="input--lg" />
+              </Form.Group>
+              <button type="submit" disabled={loading} className="btn--primary">
+                Login
+              </button>
+            </Form>
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+          <p>New to use?</p>
+          <p id="link-to-signup"><Link to="/signup">Sign Up</Link></p>
+        </div>
       </div>
+
     </>
   )
 }
