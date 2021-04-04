1df44e66fbcd17f428e22b17e664507462f02b2a
Shinya Sato <0113shin.s@gmail.com>
2021-04-03 18:55:10 -0700

    service worker setup

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './style.css'
 import App from './components/App'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals'
-import { db } from './firebase/index'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,19 +12,12 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
-
-db.enablePersistence().catch(err => {
-  if (err.code == 'failed-precondition') {
-    // Multiple tabs open, persistence can only be enabled
-    // in one tab at a a time.
-    // ...
-  } else if (err.code == 'unimplemented') {
-    // The current browser does not support all of the
-    // features required to enable persistence
-    // ...
-  }
-})
