9da35a78a830031bd43de6848a99a858e4c1962a
Shinya Sato <0113shin.s@gmail.com>
2021-04-10 19:29:26 -0700

    modify error and success messages to be shown according to result

--- src/components/EditReview.js
+++ src/components/EditReview.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import { db, storage } from '../firebase/index'
-import { Form, Alert } from 'react-bootstrap'
-import drinkCategories from '../lib/drinkCategories'
-import ReactStars from 'react-rating-stars-component'
 import { useHistory } from 'react-router-dom'
+import ReactStars from 'react-rating-stars-component'
+import { Alert } from 'react-bootstrap'
+import { db, storage } from '../firebase/index'
+import drinkCategories from '../lib/drinkCategories'
 
 export default function EditReview() {
   const [review, setReview] = useState('')
@@ -14,231 +14,170 @@ export default function EditReview() {
   const [comment, setComment] = useState('')
   const [photoURL, setPhotoURL] = useState('')
   const [image, setImage] = useState(null)
-  let review_id = window.location.pathname.split('/review/edit/', 2)[1]
   const [error, setError] = useState('')
   const [message, setMessage] = useState('')
+
   const history = useHistory()
 
+  const review_id = window.location.pathname.split('/review/edit/', 2)[1]
+  const reviewRef = db.collection('reviews').doc(review_id)
+
   useEffect(() => {
-    const reviewRef = db.collection('reviews').doc(review_id)
     reviewRef.get().then(doc => {
-      const imageFulPath = doc.data().fullPath
-      setReview(doc.data())
-      setDrinkName(doc.data().drink_name)
-      setDrinkCategory(doc.data().drink_category)
-      setPrice(doc.data().price)
-      // console.log(doc.data().rating)
-      setRating(doc.data().rating)
-      setComment(doc.data().comment)
+      const reviewData = doc.data()
+      const imageFulPath = reviewData.fullPath
+
+      setReview(reviewData)
+      setDrinkName(reviewData.drink_name)
+      setDrinkCategory(reviewData.drink_category)
+      setPrice(reviewData.price)
+      setRating(reviewData.rating)
+      setComment(reviewData.comment)
+
       storage
         .ref(imageFulPath)
         ?.getDownloadURL()
-        .then(url => {
-          setPhotoURL(url)
-        })
+        .then(url => setPhotoURL(url))
     })
   }, [])
 
-  const inputDrinkName = useCallback(
-    event => {
-      setDrinkName(event.target.value)
-    },
-    [setDrinkName]
-  )
+  const handleDrinkNameChange = useCallback(e => setDrinkName(e.target.value), [setDrinkName])
+  const handleDrinkCategoryChange = useCallback(e => setDrinkCategory(e.target.value), [
+    setDrinkCategory,
+  ])
+  const handlePriceChange = useCallback(e => setPrice(parseFloat(e.target.value)), [setPrice])
+  const handleRatingChange = useCallback(e => setRating(parseInt(e)), [setRating])
+  const handleCommentChange = useCallback(e => setComment(e.target.value), [setComment])
+  const handleImageChange = e => {
+    setImage(e.target.files[0])
+    setPhotoURL(window.URL.createObjectURL(e.target.files[0]))
+  }
 
-  const inputDrinkCategory = useCallback(
-    event => {
-      setDrinkCategory(event.target.value)
-    },
-    [setDrinkCategory]
-  )
-
-  const inputPrice = useCallback(
-    event => {
-      setPrice(event.target.value)
-    },
-    [setPrice]
-  )
-
-  const inputRating = useCallback(
-    event => {
-      setRating(parseInt(event))
-    },
-    [setRating]
-  )
-
-  const inputComment = useCallback(
-    event => {
-      setComment(event.target.value)
-    },
-    [setComment]
-  )
-
-  const updateReview = e => {
+  const handleUpdate = e => {
     e.preventDefault()
-    if (image !== null) {
-      let randomID = Math.random().toString(32).substring(2)
-      const fullPath = 'reviewPhoto/' + randomID + image.name
-      let storageRef = storage.ref().child(fullPath)
-      storageRef
-        .put(image)
-        .then(res => {
-          db.collection('reviews')
-            .doc(review_id)
-            .update({ fullPath: fullPath })
-            .catch(error => {
-              console.log(error)
-            })
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    }
-    const promises = []
+
     setError('')
     setMessage('')
 
-    //drink name,category price etc,,,
-    promises.push(
-      db
-        .collection('reviews')
-        .doc(review_id)
-        .get()
-        .then(doc => {
-          if (doc.data().drink_name != drinkName) {
-            db.collection('reviews').doc(review_id).update({ drink_name: drinkName })
-          }
-          if (doc.data().drinkcategory != drinkCategory) {
-            db.collection('reviews').doc(review_id).update({ drinkcategory: drinkCategory })
-          }
-          if (doc.data().price != price) {
-            db.collection('reviews').doc(review_id).update({ price: price })
-          }
-          if (doc.data().rating != rating) {
-            db.collection('reviews').doc(review_id).update({ rating: rating })
-          }
-          if (doc.data().comment != comment) {
-            db.collection('reviews').doc(review_id).update({ comment: comment })
-          }
-        })
-    )
+    if (image !== null) {
+      const randomID = Math.random().toString(32).substring(2)
+      const fullPath = 'reviewPhoto/' + randomID + image.name
+      const storageRef = storage.ref().child(fullPath)
 
-    Promise.all(promises)
-      .then(() => {
-        setMessage('Review was successfully updated')
+      storageRef
+        .put(image)
+        .then(res => reviewRef.update({ fullPath }).catch(err => console.log(err)))
+        .catch(err => console.log('Failed to download image', err))
+    }
+
+    reviewRef
+      .update({
+        drink_name: drinkName,
+        drink_category: drinkCategory,
+        price,
+        rating,
+        comment,
       })
-      .catch(() => {
+      .then(() => setMessage('Review is successfully updated!'))
+      .catch(err => {
         setError('Failed to update review')
+        console.log('Failed to update review: ', err)
       })
   }
 
-  const handleImageChange = event => {
-    const { files } = event.target
-    setImage(event.target.files[0])
-    setPhotoURL(window.URL.createObjectURL(files[0]))
-  }
-
-  function handleDelete() {
-    if (window.confirm('Are you Sure to Delete This Review?')) {
-      db.collection('reviews')
-        .doc(review_id)
-        .delete()
-        .then(() => {
-          console.log('Document successfully deleted!')
-        })
-        .catch(error => {
-          console.error('Error removing document: ', error)
-        })
+  const handleDelete = () => {
+    if (window.confirm('Are you sure to delete this review?')) {
       setDrinkName('')
       setDrinkCategory('')
       setPrice('')
       setRating('')
       setComment('')
       setPhotoURL('')
-
-      const promises = []
       setError('')
       setMessage('')
-      Promise.all(promises)
+
+      reviewRef
+        .delete()
         .then(() => {
           setMessage('Review was successfully deleted')
           setTimeout(() => history.push('/'), 1500)
         })
-        .catch(() => {
+        .catch(err => {
           setError('Failed to delete review')
+          console.error('Failed to delete review: ', err)
         })
     }
   }
 
   return (
-    <>
-      <Form className="form edit-review" onSubmit={updateReview}>
-        <input
-          label="Drink name"
-          required={true}
-          rows={1}
-          value={drinkName}
-          type="text"
-          onChange={inputDrinkName}
-          placeholder="Drink name"
-        />
+    <form className="edit-review" onSubmit={handleUpdate}>
+      <input
+        label="Drink name"
+        required={true}
+        rows={1}
+        value={drinkName}
+        type="text"
+        onChange={handleDrinkNameChange}
+        placeholder="Drink name"
+      />
 
-        <div className="select-container">
-          <select required={true} value={drinkCategory} onChange={inputDrinkCategory}>
-            <option value="">Select drink category</option>
-            {drinkCategories.map(category => (
-              <option key={category} value={category}>
-                {category}
-              </option>
-            ))}
-          </select>
-        </div>
+      <div className="select-container">
+        <select required={true} value={drinkCategory} onChange={handleDrinkCategoryChange}>
+          <option value="">Select drink category</option>
+          {drinkCategories.map(category => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
 
-        <input
-          label="Price"
-          max="99999"
-          required={true}
-          rows={1}
-          value={price}
-          type="number"
-          onChange={inputPrice}
-          placeholder="Price"
-        />
+      <input
+        label="Price"
+        max="99999"
+        required={true}
+        rows={1}
+        value={price}
+        type="number"
+        onChange={handlePriceChange}
+        placeholder="Price"
+      />
 
-        <ReactStars
-          count={5}
-          value={rating}
-          onChange={inputRating}
-          size={24}
-          activeColor="#de9e48"
-        />
+      <ReactStars
+        count={5}
+        value={rating}
+        onChange={handleRatingChange}
+        size={24}
+        activeColor="#de9e48"
+      />
 
-        <textarea
-          className="comment"
-          required={true}
-          value={comment}
-          placeholder="Please write a review"
-          name="comment"
-          rows="4"
-          cols="40"
-          onChange={inputComment}
-        />
+      <textarea
+        className="comment"
+        required={true}
+        value={comment}
+        placeholder="Please write a review"
+        name="comment"
+        rows="4"
+        cols="40"
+        onChange={handleCommentChange}
+      />
 
-        <div className="image_wrapper">
-          <input type={'file'} onChange={handleImageChange} />
-          <img src={photoURL} className="w-100" />
-        </div>
-        {message && <Alert variant="success">{message}</Alert>}
-        {error && <Alert variant="danger">{error}</Alert>}
-        <div className="btn-area--half">
-          <button className="btn--secondary btn--half" type="submit">
-            Update
-          </button>
-          <button className="btn--primary btn--half" label="Delete" onClick={() => handleDelete()}>
-            Delete
-          </button>
-        </div>
-      </Form>
-    </>
+      <div className="image_wrapper">
+        <input type="file" onChange={handleImageChange} />
+        <img src={photoURL} className="w-100" alt="drink" />
+      </div>
+
+      {message && <Alert variant="success">{message}</Alert>}
+      {error && <Alert variant="danger">{error}</Alert>}
+
+      <div className="btn-area--half">
+        <button className="btn--secondary btn--half" type="submit">
+          Update
+        </button>
+        <button className="btn--primary btn--half" label="Delete" onClick={handleDelete}>
+          Delete
+        </button>
+      </div>
+    </form>
   )
 }
